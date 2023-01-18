d701191c51a9032edd80c0f8ac3a5f9743e30eb2
Vernon2025 <112723885+Vernon2025@users.noreply.github.com>
2023-01-17 20:26:08 -0800

    Closer Done

--- PDA.java
+++ PDA.java
@@ -1,22 +1,19 @@
-
-import java.util.Scanner;
-import java.util.InputMismatchException;
 /**
  * Permissible Dating Age program
  *
  * @author Mr. Jaffe
  * @version 2021-06-22 Version 1.0.0
  */
-public class PDA
-{
+import java.util.Scanner;
+import java.util.InputMismatchException;
+public class PDA {
     int age;
     int LOWER_BOUND = 14;
-    boolean shouldContinue = true;
+    static boolean shouldContinue = true;
     /**
      * Constructor for objects of class PDA
      */
-    public PDA()
-    {
+    public PDA() {
         // We don't need to do anything in the constructor for
         // our program.
     }
@@ -27,34 +24,27 @@ public class PDA
         Scanner scanner = new Scanner(System.in);
         while (shouldContinue) {
             System.out.println("How old are you?");
-            System.out.println("Type 0 to quit");
+            System.out.println("Enter age as 0 to quit");
             try {
                 age = scanner.nextInt();
                 if (age < LOWER_BOUND) {
                     System.out.println(age + " is too young!!");
                 } else {
-                    System.out.println("Oldest: " + (age/2 + 7));
-                    System.out.println("Youngest: " + (age/2 - 7));
+                    getYoungerAndOlderAge(age);
                 }
             } catch (InputMismatchException error) {
-                System.out.println("Please enter an integer");
                 scanner.next();
+                System.out.println("Please enter an integer");
             }
             if (age == 0) {
                 shouldContinue = false;
-                System.out.println("This program closed");
             }
         }
     }
-    public void getYoungerAge(int age){
-        System.out.println("Minimum Age: ");
+    public static void getYoungerAndOlderAge (int age) {
+        System.out.println("The minimun age is " + ((age/2) + 7));
+        System.out.println("The maximum age is " + ((age*2) - 7));
     }
-    public void getOlderAge(int age) {
-        System.out.println("Maximum Age: " );
-    }
-    /**
-     * The main method instantiates and runs the program
-     */
     public static void main (String[] args) {
         PDA pda = new PDA();
         pda.runEventLoop();
